6ed7d11177141c4f617c4a2565a5232927f172a0
Jayesh23 <iamjayesh23@gmail.com>
2017-01-17 01:36:50 +0530

    Add files via upload
    ultrasonic sensor added.

---
+++ Sonar_Data.ino
@@ -0,0 +1,32 @@
+/*
+ PROJECT: SensoDuino 0.17 
+ PROGRAMMER: Hazim Bitar (techbitar at gmail dot com)
+ DATE: Sep 30, 2013
+ FILE: sensoduino.ino
+ LICENSE: Public domain
+ */
+
+// Replace sensor numbers with Names
+#include<NewPing.h>
+
+
+#define TRIGGER_PIN   12
+#define ECHO_PIN      11
+#define MAX_DISTANCE 200
+
+NewPing sonar( TRIGGER_PIN , ECHO_PIN , MAX_DISTANCE );
+
+void setup() {
+  Serial.begin(9600);
+  }
+
+void loop()
+{
+    unsigned int uS = sonar.ping();
+    Serial.print(uS);
+    Serial.println("-----------------------");
+    delay(200);
+  }
+
+
+
